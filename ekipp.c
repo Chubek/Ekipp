4a78411890d0729970d0df24fa8f28602379bf83
Chubak Bidpaa <chubakbidpaa@gmail.com>
2023-10-27 04:10:08 +0330

    fixed: locality

--- ekipp.c
+++ ekipp.c
@@ -14,23 +14,18 @@
 #include <sys/stat.h>
 #include <sys/sysmacros.h>
 
-#define Local                   static
-#define Inline                  static inline
-#define External                extern
-
 #include "ekipp.h"
-
 #include "errors.gen"
 
 #define EEXIT(e, c) do { fputs(e, stderr); exit(c);  } while (0)
 
-Local struct LinkedList {
+static struct LinkedList {
 	struct LinkedList*		next;
 	wchar_t*			name;
 	wchar_t*			value;
 } *Symtable;
 
-Inline void insert_symbol(wchar_t* name, wchar_t* value) {
+void insert_symbol(wchar_t* name, wchar_t* value) {
 	node_t* 	node	= calloc(1, sizeof(node_t));
 	node->next	= Symtable;
 	node->name	= wcsdup(name);
@@ -40,7 +35,7 @@ Inline void insert_symbol(wchar_t* name, wchar_t* value) {
 	Symtable	= node;
 }
 
-Inline wchar_t* get_symbol(wchar_t* name) {
+wchar_t* get_symbol(wchar_t* name) {
 	node_t* 	node;
 	size_t 		len  = wcslen(name);
 
@@ -51,13 +46,13 @@ Inline wchar_t* get_symbol(wchar_t* name) {
 	return NULL;
 }
 
-Inline void free_node(node_t* node) {
+void free_node(node_t* node) {
 	free(node->name);
 	free(node->value);
 	free(node);
 }
 
-Inline void remove_symbol(wchar_t* name) {
+void remove_symbol(wchar_t* name) {
 	node_t* 	node;
 	size_t 		len  = wcslen(name);
 
@@ -70,7 +65,7 @@ Inline void remove_symbol(wchar_t* name) {
 
 }
 
-Inline void dump_symtable(void) {
+void dump_symtable(void) {
 	node_t*		node;
 
 	for (node = Symtable; node != NULL; node = node->next)
@@ -79,13 +74,13 @@ Inline void dump_symtable(void) {
 
 #define MAX_STACK 	4096
 
-Local struct DefStack {
+static struct DefStack {
 	wchar_t*	name;
 	wchar_t*	value;
 } Defstack[MAX_STACK];
-Local size_t	stack_pointer = 0;
+size_t	stack_pointer = 0;
 
-Inline void push_stack(wchar_t* name, wchar_t* value) {
+void push_stack(wchar_t* name, wchar_t* value) {
 	Defstack[stack_pointer].name   = wcsdup(name);
 	Defstack[stack_pointer].value  = wcsdup(value);
 	free(name);
@@ -93,7 +88,7 @@ Inline void push_stack(wchar_t* name, wchar_t* value) {
 	stack_pointer++;
 }
 
-Inline wchar_t*	pop_stack_value(wchar_t* name) {
+wchar_t*	pop_stack_value(wchar_t* name) {
 	size_t len = wcslen(name);
 	size_t ptr = stack_pointer;
 
@@ -105,11 +100,11 @@ Inline wchar_t*	pop_stack_value(wchar_t* name) {
 	return NULL;
 }
 
-Inline wchar_t* stack_pop(void) {
+wchar_t* stack_pop(void) {
 	return Defstack[--stack_pointer].value;
 }
 
-Inline void dump_stack(void) {
+void dump_stack(void) {
 	while (--stack_pointer) {
 		free(Defstack[stack_pointer].name);
 		free(Defstack[stack_pointer].value);
@@ -119,17 +114,17 @@ Inline void dump_stack(void) {
 
 #define NUM_DIVERT 	32
 
-Local   FILE*		divert_streams[NUM_DIVERT];
-Local 	wchar_t*	divert_strings[NUM_DIVERT];
-Local 	size_t		divert_lengths[NUM_DIVERT];
-Local	FILE*		current_divert;
-Local   DIR*		tmp_dir;
-Local	dev_t		null_dev;
-Local	int		null_fd;
-Local	FILE*		null_divert;
-Local	int		current_divert_idx;
-Local	FILE*		output;
-Local	FILE*		hold;
+FILE*		divert_streams[NUM_DIVERT];
+wchar_t*	divert_strings[NUM_DIVERT];
+size_t		divert_lengths[NUM_DIVERT];
+FILE*		current_divert;
+DIR*		tmp_dir;
+dev_t		null_dev;
+int		null_fd;
+FILE*		null_divert;
+int		current_divert_idx;
+FILE*		output;
+FILE*		hold;
 
 #define OUTPUT(ws) 		(fputws(ws, output))
 #define OUTPUT_DIVERT(ws) 	(fputws(ws, current_divert))
@@ -139,7 +134,7 @@ Local	FILE*		hold;
 
 #define NULL_NAME "ekippnull"
 
-Inline void open_null_file(void) {
+void open_null_file(void) {
 	tmp_dir = opendir(P_tmpdir);
 	null_dev = makedev(MAJOR_NULL, MINOR_NULL);
 	if ((null_fd = mknodat(dirfd(tmp_dir), 
@@ -151,7 +146,7 @@ Inline void open_null_file(void) {
 	null_divert = fdopen(null_fd, "w");
 }
 
-Inline void destroy_null_divert(void) {
+void destroy_null_divert(void) {
 	if (null_divert) {
 		fclose(null_divert);
 		close(null_fd);
@@ -159,7 +154,7 @@ Inline void destroy_null_divert(void) {
 	}
 }
 
-Inline void set_divert(int n) {
+void set_divert(int n) {
 	if (n > NUM_DIVERT) {
 		EEXIT(ERR_NUM_DIVERT, ECODE_NUM_DIVERT);
 	}
@@ -179,7 +174,7 @@ Inline void set_divert(int n) {
 }
 
 
-Inline void unset_divert(int n) {
+void unset_divert(int n) {
 	if (n < 0) {
 		EEXIT(ERR_UNSET_NULLDIV, ECODE_UNSET_NULLDIV);
 	} else if (n >= NUM_DIVERT) {
@@ -191,7 +186,7 @@ Inline void unset_divert(int n) {
 	}
 }
 
-Inline void free_set_diverts(void) {
+void free_set_diverts(void) {
 	int i = NUM_DIVERT;
 	while (--i) {
 		if (divert_streams[i]) {
@@ -203,24 +198,24 @@ Inline void free_set_diverts(void) {
 	destroy_null_divert();
 }
 
-Inline void switch_output(FILE* stream) {
+void switch_output(FILE* stream) {
 	hold 	= output;
 	output  = stream;
 }
 
-Inline void unswitch_output(void) {
+void unswitch_output(void) {
 	output	= hold;
 }
 
 #define NMATCH		1
-Local 	regex_t		reg_cc;
-Local	regmatch_t	reg_pmatch[NMATCH];
-Local	char*		reg_input;
-Local	char*		reg_pattern;
-Local	wchar_t*	reg_matchmsg;
-Local	wchar_t*	reg_nomatchmsg;
+	regex_t		reg_cc;
+regmatch_t	reg_pmatch[NMATCH];
+char*		reg_input;
+char*		reg_pattern;
+wchar_t*	reg_matchmsg;
+wchar_t*	reg_nomatchmsg;
 
-Inline void ifelse_regmatch(void) {
+void ifelse_regmatch(void) {
 	if (regcomp(&reg_cc, reg_pattern, REG_NOSUB) < 0) {
 		EEXIT(ERR_REG_COMP, ECODE_REG_COMP);
 	}
@@ -236,7 +231,7 @@ Inline void ifelse_regmatch(void) {
 	regfree(&reg_cc);
 }
 
-Inline void search_file(FILE* stream) {
+void search_file(FILE* stream) {
 	wchar_t* line_str;
 	wchar_t* word;
 	size_t	 line_len;
@@ -269,7 +264,7 @@ Inline void search_file(FILE* stream) {
 
 }
 
-Inline void open_search_close(char* path) {
+void open_search_close(char* path) {
 	FILE* stream = fopen(path, "r");
 	search_file(stream);
 	fclose(stream);
@@ -278,19 +273,19 @@ Inline void open_search_close(char* path) {
 
 extern FILE* yyin;
 
-Inline void yyin_search(void) {
+void yyin_search(void) {
 	FILE* yyin_cpy = yyin;
 	search_file(yyin_cpy);
 }
 
 #define FLUSH_STDIO() (fflush(stdin), fflush(stdout), fflush(stderr))
 
-Local	char*		exec_cmd;
-Local	wchar_t*	exec_strcmp;
-Local	wchar_t*   	exec_strne;
-Local	wchar_t*	exec_streq;
+char*		exec_cmd;
+wchar_t*	exec_strcmp;
+wchar_t*   	exec_strne;
+wchar_t*	exec_streq;
 
-Inline void ifelse_execmatch(void) {
+void ifelse_execmatch(void) {
 	FILE* pipe;
 	FLUSH_STDIO();
 	if (!(pipe = popen(exec_cmd, "r"))) {
@@ -319,7 +314,7 @@ Inline void ifelse_execmatch(void) {
 	free(exec_streq);
 }
 
-Inline void exec_command(void) {
+void exec_command(void) {
 	FILE* stream = popen(exec_cmd, "r");
 	if (!stream) {
 		EEXIT(ERR_EXEC_CMD, ECODE_EXEC_CMD);	
@@ -347,13 +342,13 @@ Inline void exec_command(void) {
 	free(exec_cmd);
 }
 
-Local FILE*		delim_stream;
-Local char		delim_fpath[FILENAME_MAX];
-Local char*		delim_command;	
+FILE*		delim_stream;
+char		delim_fpath[FILENAME_MAX];
+char*		delim_command;	
 
 #define OUTPUT_DELIM(ws) (fputws(ws, delim_stream))
 
-Inline void init_delim_stream(wchar_t* text, size_t len) {
+void init_delim_stream(wchar_t* text, size_t len) {
 	memset(&delim_fpath[0], 0, FILENAME_MAX);
 	delim_fpath[0] = 'X'; delim_fpath[1] = 'X'; delim_fpath[2] = 'X';
 	delim_fpath[3] = 'X'; delim_fpath[4] = 'X'; delim_fpath[5] = 'E';
@@ -370,7 +365,7 @@ Inline void init_delim_stream(wchar_t* text, size_t len) {
 	}
 }
 
-Inline void exec_delim_command(void) {
+void exec_delim_command(void) {
 	FLUSH_STDIO();
 	fclose(delim_stream);
 	if (!(delim_stream = freopen(&delim_fpath[0], "r", stdin))) {
@@ -399,31 +394,31 @@ Inline void exec_delim_command(void) {
 #define MAX_TOKEN	8
 #define REGISTRY_SIZE	65536
 
-Local	char	quote_left[MAX_TOKEN];
-Local	char	quote_right[MAX_TOKEN];
-Local	char	comment_left[MAX_TOKEN];
-Local	char	comment_right[MAX_TOKEN];
-Local 	char	delim_left[MAX_TOKEN];
-Local	char	delim_right[MAX_TOKEN];
-Local	char	argnum_sigil[MAX_TOKEN];
-Local	char	engage_sigil[MAX_TOKEN];
-Local	char	cond_sigil[MAX_TOKEN];
-Local	char	search_sigil[MAX_TOKEN];
-Local	char	aux_sigil[MAX_TOKEN];
+char	quote_left[MAX_TOKEN];
+char	quote_right[MAX_TOKEN];
+char	comment_left[MAX_TOKEN];
+char	comment_right[MAX_TOKEN];
+	char	delim_left[MAX_TOKEN];
+char	delim_right[MAX_TOKEN];
+char	argnum_sigil[MAX_TOKEN];
+char	engage_sigil[MAX_TOKEN];
+char	cond_sigil[MAX_TOKEN];
+char	search_sigil[MAX_TOKEN];
+char	aux_sigil[MAX_TOKEN];
 
 #define MAX_FMT		MAX_TOKEN * 8
 
-Local    char fmt_delim[MAX_FMT]; 
-Local    char fmt_comment[MAX_FMT];
-Local    char fmt_quote[MAX_FMT];
+   char fmt_delim[MAX_FMT]; 
+   char fmt_comment[MAX_FMT];
+   char fmt_quote[MAX_FMT];
 
-Local	bool	tokens_registry[REGISTRY_SIZE];
+bool	tokens_registry[REGISTRY_SIZE];
 
-Inline void zero_registry(void) {
+void zero_registry(void) {
 	memset(&tokens_registry[0], false, sizeof(bool) * REGISTRY_SIZE);
 }
 
-Inline void register_token(char* token) {
+void register_token(char* token) {
 	uint16_t hash = 0;
 	char	 c;
 	
@@ -437,16 +432,16 @@ Inline void register_token(char* token) {
 	tokens_registry[hash] = true;
 }
 
-Inline void set_token(char* token, char* value) {
+void set_token(char* token, char* value) {
 	register_token(value);
 	memset(&token[0], 0, MAX_TOKEN);
 	memmove(&token[0], &value[0], MAX_TOKEN);
-	reformats_fmts();
+	reformat_fmts();
 	free(token);
 	free(value);
 }
 
-Inline void reformat_fmts(void) {
+void reformat_fmts(void) {
 	sprintf(&fmt_delim[0], 
 			"%s%%s%s", &delim_left[0], &delim_right[0]);
 	
@@ -457,42 +452,42 @@ Inline void reformat_fmts(void) {
 			"%s%%s%s", &quote_left[0], &quote_right[0]);
 }
 
-Inline bool token_is(char* token, char* cmp, size_t len) {
+bool token_is(char* token, char* cmp, size_t len) {
 	return !strncmp(token, cmp, len);
 }
 
 
-External  void 		yyinvoke(wchar_t* code);
-External  char		keyletter;
+extern void 		yyinvoke(wchar_t* code);
+extern char		keyletter;
 
 #ifndef ARG_MAX
 #define ARG_MAX		1024
 #endif
 
-Local	  wchar_t* 	invoke_argv[ARG_MAX];
-Local	  size_t	invoke_argc = 0;
-Local	  size_t	invoke_argn = 0;
+wchar_t* 	invoke_argv[ARG_MAX];
+size_t	invoke_argc = 0;
+size_t	invoke_argn = 0;
 
-Inline void invoke_addarg(wchar_t* arg) {
+void invoke_addarg(wchar_t* arg) {
 	invoke_argv[invoke_argc++] = wcsdup(arg);
 	free(arg);
 }
 
-Inline wchar_t* invoke_getarg(size_t n) {
+wchar_t* invoke_getarg(size_t n) {
 	return invoke_argv[n];
 }
 
-Inline void invoke_printnext(void) {
+void invoke_printnext(void) {
 	OUTPUT(invoke_argv[invoke_argn++]);
 }
 
-Inline void invoke_printarg(size_t n) {
+void invoke_printarg(size_t n) {
 	if (n < invoke_argc) {
 		OUTPUT(invoke_argv[n]);
 	}
 }
 
-Inline void invoke_printargs(wchar_t* delim) {
+void invoke_printargs(wchar_t* delim) {
 	size_t n = 0;
 	while (n < invoke_argc - 1) {
 		OUTPUT(invoke_argv[n++]);
@@ -503,14 +498,14 @@ Inline void invoke_printargs(wchar_t* delim) {
 	free(delim);
 }
 
-Inline void invoke_dumpargs(void) {
+void invoke_dumpargs(void) {
 	while (--invoke_argc)
 		free(invoke_argv[invoke_argc]);
 	memset(&invoke_argv[0], 0, ARG_MAX * sizeof(wchar_t*));
 	invoke_argn = 0;
 }
 
-Inline void invoke_macro(wchar_t *id) {
+void invoke_macro(wchar_t *id) {
 	wchar_t* macro = get_symbol(id);
 	if (!macro)
 		macro = pop_stack_value(id);
@@ -521,9 +516,9 @@ Inline void invoke_macro(wchar_t *id) {
 	free(id);
 }
 
-Local wchar_t*	fmt;
+wchar_t*	fmt;
 
-Inline void print_formatted(void) {
+void print_formatted(void) {
 	wchar_t wc;
 	wchar_t f[3] = {0};
 	int i = 0;
@@ -539,7 +534,7 @@ Inline void print_formatted(void) {
 }
 
 
-Inline void print_env(char* key) {
+void print_env(char* key) {
 	char* var;
 	if ((var = getenv(key)) != NULL)
 		fputs(var, output);
@@ -549,25 +544,25 @@ Inline void print_env(char* key) {
 extern char** sys_argv;
 extern int    sys_argc;
 
-Inline void print_argv(int n) {
+void print_argv(int n) {
 	if (sys_argc < n) {
 		fputs(sys_argv[n], output);
 	}
 }
 
-Local wchar_t*	aux_prim;
-Local wchar_t*	aux_sec;
-Local wchar_t*	aux_tert;
+wchar_t*	aux_prim;
+wchar_t*	aux_sec;
+wchar_t*	aux_tert;
 
-Inline void set_aux(wchar_t** aux, wchar_t* value) {
+void set_aux(wchar_t** aux, wchar_t* value) {
 	*aux = wcsdup(value);
 }
 
-Inline void free_aux(wchar_t* aux) {
+void free_aux(wchar_t* aux) {
 	free(aux);
 }
 
-Inline void translit(int action) {
+void translit(int action) {
 	#define INPUT 		aux_prim
 	#define SRCMAP		aux_sec
 	#define DSTMAP		aux_tert
@@ -595,7 +590,7 @@ Inline void translit(int action) {
 	#undef DSTMAP
 }
 
-Inline void offset(void) {
+void offset(void) {
 	#define INPUT		aux_prim
 	#define SUB		aux_sec
 
@@ -605,7 +600,7 @@ Inline void offset(void) {
 	#undef SUB
 }
 
-Inline void list_dir(void) {
+void list_dir(void) {
 	#define DIR_PATH 	aux_prim
 
 	DIR* stream = opendir((char*)DIR_PATH);
@@ -631,7 +626,7 @@ Inline void list_dir(void) {
 	#undef DIR_PATH
 }
 
-Inline void cat_file(void) {
+void cat_file(void) {
 	#define FILE_PATH	aux_prim
 
 	FILE*	stream	= fopen((char*)FILE_PATH, "r");
@@ -662,11 +657,11 @@ Inline void cat_file(void) {
 	#undef FILE_PATH
 }
 
-Inline void include_file(void) { cat_file(); }
+void include_file(void) { cat_file(); }
 
 #define OUT_TIME_MAX 1024
 
-Inline void format_time(void) {
+void format_time(void) {
 	#define FMT	aux_prim
 
 	char out_time[OUT_TIME_MAX];
@@ -690,7 +685,7 @@ Inline void format_time(void) {
 	#undef FMT
 }
 
-Local void dnl(void) {
+void dnl(void) {
 	fscanf(yyin, "%*s\n", NULL);
 }
 
