52111b442b97cda7dd46e0a6f64be139080c0dac
Chubak Bidpaa <chubakbidpaa@gmail.com>
2023-10-27 04:10:23 +0330

    fixed: locality in declrations

--- ekipp.h
+++ ekipp.h
@@ -2,63 +2,62 @@
 #define EKIPP_H_
 
 #include <wchar.h>
+#include <stdbool.h>
 #include <sys/types.h>
 
-#ifndef Inline
-#define Inline static inline
-#endif
 
 typedef struct LinkedList 	node_t;
 typedef struct Defstack	  	dstack_t;
 
-Inline void insert_symbol(wchar_t* name, wchar_t* value); 
-Inline wchar_t* get_symbol(wchar_t* name); 
-Inline void free_node(node_t* node); 
-Inline void remove_symbol(wchar_t* name); 
-Inline void dump_symtable(void); 
-Inline void push_stack(wchar_t* name, wchar_t* value); 
-Inline wchar_t*	pop_stack(wchar_t* name); 
-Inline wchar_t* get_stack_pop(void); 
-Inline void dump_stack(void); 
-Inline void open_null_file(void); 
-Inline void destroy_null_divert(void); 
-Inline void set_divert(int n); 
-Inline void unset_divert(int n); 
-Inline void free_set_diverts(void); 
-Inline void switch_output(FILE* stream); 
-Inline void unswitch_output(void); 
-Inline void ifelse_regmatch(void); 
-Inline void search_file(FILE* stream); 
-Inline void open_search_close(char* path); 
-Inline void yyin_search(void); 
-Inline void ifelse_execmatch(void); 
-Inline void exec_command(void); 
-Inline void init_delim_stream(wchar_t* text, size_t len); 
-Inline void exec_delim_command(void); 
-Inline void zero_registry(void); 
-Inline void register_token(char* token); 
-Inline void set_token(char* token, char* value); 
-Inline void reformat_fmts(void); 
-Inline bool token_is(char* token, char* cmp, size_t len); 
-Inline void invoke_addarg(wchar_t* arg); 
-Inline wchar_t* invoke_getarg(size_t n); 
-Inline void invoke_printnext(void); 
-Inline void invoke_printarg(size_t n); 
-Inline void invoke_printargs(wchar_t* delim); 
-Inline void invoke_dumpargs(void); 
-Inline void invoke_macro(wchar_t *id); 
-Inline void foreach_macro(wchar_t* macro); 
-Inline void print_formatted(void); 
-Inline void print_env(char* key); 
-Inline void print_argv(int n); 
-Inline void set_aux(wchar_t** aux, wchar_t* value); 
-Inline void free_aux(wchar_t* aux); 
-Inline void translit(int action); 
-Inline void offset(void); 
-Inline void list_dir(void); 
-Inline void cat_file(void); 
-Inline void include_file(void);
-Inline void format_time(void); 
-
+void insert_symbol(wchar_t* name, wchar_t* value); 
+wchar_t* get_symbol(wchar_t* name); 
+void free_node(node_t* node); 
+void remove_symbol(wchar_t* name); 
+void dump_symtable(void); 
+void push_stack(wchar_t* name, wchar_t* value); 
+wchar_t* pop_stack(wchar_t* name); 
+wchar_t* get_stack_pop(void); 
+void dump_stack(void); 
+void open_null_file(void); 
+void destroy_null_divert(void); 
+void set_divert(int n); 
+void unset_divert(int n); 
+void free_set_diverts(void); 
+void switch_output(FILE* stream); 
+void unswitch_output(void); 
+void ifelse_regmatch(void); 
+void search_file(FILE* stream); 
+void open_search_close(char* path); 
+void yyin_search(void); 
+void ifelse_execmatch(void); 
+void exec_command(void); 
+void init_delim_stream(wchar_t* text, size_t len); 
+void exec_delim_command(void); 
+void zero_registry(void); 
+void register_token(char* token); 
+void set_token(char* token, char* value); 
+void reformat_fmts(void); 
+bool token_is(char* token, char* cmp, size_t len); 
+void invoke_addarg(wchar_t* arg); 
+wchar_t* invoke_getarg(size_t n); 
+void invoke_printnext(void); 
+void invoke_printarg(size_t n); 
+void invoke_printargs(wchar_t* delim); 
+void invoke_dumpargs(void); 
+wchar_t* invoke_joinargs(wchar_t* delim);
+void invoke_macro(wchar_t *id); 
+void foreach_macro(wchar_t* macro); 
+void print_formatted(void); 
+void print_env(char* key); 
+void print_argv(int n); 
+void set_aux(wchar_t** aux, wchar_t* value); 
+void free_aux(wchar_t* aux); 
+void translit(int action); 
+void offset(void); 
+void list_dir(void); 
+void cat_file(void); 
+void include_file(void);
+void format_time(void); 
+void dnl(void);
 
 #endif
